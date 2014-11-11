cb82a7d7c014f877b725db48a46fea29bfa58d7b
jwsmithers <joshuasmee@hotmail.com>
2014-11-11 11:16:24 +0200

    added rest

--- BuildAtlasOffline.sh
+++ BuildAtlasOffline.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+################################ House Keeping ################################
 boxes WelcomeMSG.txt
 
 echo "Setting up your directory structure..."
@@ -6,25 +7,56 @@ source Environment.sh
 echo "You've identified your system as $CMTCONFIG"
 
 alias goHome="cd $TopDir"
-
+################################ Create the main projects and their project.cmt files ################################
 while read line 
 do
         cmt create $TopDir/$line $line-$VERSION
-	#alias $line="cd $TopDir/$line $line-$VERSION"
-	cd $TopDir/$line/$line-$VERSION/cmt
-	rm ./*
-	echo "$line" >> project.cmt
+        rm $TopDir/$line/$line-$VERSION/cmt/*
+        echo "project $line" >> $TopDir/$line/$line-$VERSION/cmt/project.cmt
+        alias $line="cd $TopDir/$line/$line-$VERSION"
 done < Projects.txt
 
+################################ Create the Release files for each project and the requirements files ################################
+echo "Creating Release packages..."
 
-python <<END_OF_PYTHON
-#!/usr/bin/env python
+function CreateRequirements {
+	echo "package $2" > ./$2/$3/cmt/requirements
+        python ../../../CreateRequirementsfile.py ../../../SVN/$1 >> ./$2/$3/cmt/requirements
+}
 
-import sys
 
-print ("xyzzy")
 
-sys.exit(0)
-END_OF_PYTHON
+DetCommon && cmt create DetCommonRelease DetCommonRelease-DetCommon-00-01-52
+CreateRequirements "SVNDetCommon.txt" "DetCommonRelease" "DetCommonRelease-DetCommon-00-01-52"
+
+AtlasCore && cmt create  AtlasCoreRelease AtlasCoreRelease-AtlasCore-00-02-16
+CreateRequirements "SVNAtlasCore.txt" "AtlasCoreRelease" "AtlasCoreRelease-AtlasCore-00-02-16"
+
+AtlasConditions && cmt create AtlasConditionsRelease AtlasConditionsRelease-AtlasConditions-00-02-11
+CreateRequirements "SVNAtlasConditions.txt" "AtlasConditionsRelease" "AtlasConditionsRelease-AtlasConditions-00-02-11"
+
+AtlasEvent && cmt create AtlasEventRelease AtlasEventRelease-AtlasEvent-01-02-19
+CreateRequirements "SVNAtlasEvent.txt" "AtlasEventRelease" "AtlasEventRelease-AtlasEvent-01-02-19"
+
+AtlasReconstruction && cmt create AtlasReconstructionRelease AtlasReconstructionRelease-AtlasReconstruction-00-02-13
+CreateRequirements "SVNAtlasReconstruction.txt" "AtlasReconstructionRelease" "AtlasReconstructionRelease-AtlasReconstruction-00-02-13"
+
+AtlasSimulation && cmt create AtlasSimulationRelease AtlasSimulationRelease-AtlasSimulation-00-01-96
+CreateRequirements "SVNAtlasSimulation.txt" "AtlasSimulationRelease" "AtlasSimulationRelease-AtlasSimulation-00-01-96"
+
+AtlasTrigger && cmt create AtlasTriggerRelease AtlasTriggerRelease-AtlasTrigger-00-01-94
+CreateRequirements "SVNAtlasTrigger.txt" "AtlasTriggerRelease" "AtlasTriggerRelease-AtlasTrigger-00-01-94"
+
+AtlasAnalysis && cmt create AtlasAnalysisRelease AtlasAnalysisRelease-AtlasAnalysis-00-01-99
+CreateRequirements "SVNAtlasAnalysis.txt" "AtlasAnalysisRelease" "AtlasAnalysisRelease-AtlasAnalysis-00-01-99"
+
+AtlasOffline && cmt create AtlasOfflineRelease AtlasOfflineRelease-AtlasOffline-00-01-70
+CreateRequirements "SVNAtlasOffline.txt" "AtlasOfflineRelease" "AtlasOfflineRelease-AtlasOffline-00-01-70"
+
+AtlasHLT && cmt create AtlasHLTRelease AtlasHLTRelease-AtlasHLT-00-01-58
+CreateRequirements "SVNAtlasHLT.txt" "AtlasHLTRelease" "AtlasHLTRelease-AtlasHLT-00-01-58"
+################################################################################################################################
+
+goHome && cd ../
 
 echo "goodbye!";
