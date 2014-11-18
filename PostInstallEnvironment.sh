80f4367b742b179fcd0fead3fbbe2bdda08d4a73
jwsmithers <joshuasmee@hotmail.com>
2014-11-18 16:47:31 +0200

    more patches

--- PostInstallEnvironment.sh
+++ PostInstallEnvironment.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 export TCMALLOCDIR=/home/jwsmith/HDD/lcgcmake-install/tcmalloc/1.7p3/armv7l-fc21-gcc49-opt/lib
-export PYTHONPATH=$PYTHONPATH:$TopDir/AtlasCore/AtlasCore-18.9.0/InstallArea/python:$TopDir/AtlasCore/AtlasCore-18.9.0/Control/AthenaCommon/AthenaCommon-02-18-27/python:$ROOTDIR/Gaudi/InstallArea/$CMTCONFIG/python
+export PATH=/home/jwsmith/HDD/AtlasOfflineBuild/InstallArea/AtlasCore/AtlasCore-18.9.0/InstallArea/share/bin:/home/jwsmith/HDD/AtlasOfflineBuild/InstallArea/AtlasCore/AtlasCore-18.9.0/InstallArea/share/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/jwsmith/.local/bin:/home/jwsmith/bin
+export PYTHONPATH=/home/jwsmith/HDD/Gaudi/InstallArea/armv7l-fc21-gcc49-opt/jobOptions:/home/jwsmith/HDD/AtlasOfflineBuild/InstallArea/AtlasCore/AtlasCore-18.9.0/InstallArea/jobOptions:/home/jwsmith/HDD/AtlasOfflineBuild/InstallArea/AtlasCore/AtlasCore-18.9.0/InstallArea/python::/AtlasCore/AtlasCore-18.9.0/InstallArea/python:/AtlasCore/AtlasCore-18.9.0/Control/AthenaCommon/AthenaCommon-02-18-27/python:/home/jwsmith/HDD/Gaudi/InstallArea/armv7l-fc21-gcc49-opt/python
+export JOBOPTSEARCHPATH=/home/jwsmith/HDD/Gaudi/InstallArea/armv7l-fc21-gcc49-opt/jobOptions:/home/jwsmith/HDD/AtlasOfflineBuild/InstallArea/AtlasCore/AtlasCore-18.9.0/InstallArea/jobOptions
+source /home/jwsmith/HDD/lcgcmake-install/ROOT/v5-34-21/armv7l-fc21-gcc49-opt/bin/thisroot.sh
