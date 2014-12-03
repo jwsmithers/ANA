ba8fbc4f06b9a41daa7b69d7f6c827a5b5ae5f43
jwsmithers <joshuasmee@hotmail.com>
2014-12-03 14:45:50 +0200

    yum install instead of apt-get hacks

--- hacks/aptgethacks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo yum install hdf5-devel pyutil gperftools python-4Suite-XML gcc-plugin-devel
---
+++ hacks/yuminsthacks.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo yum install hdf5-devel pyutil gperftools python-4Suite-XML gcc-plugin-devel fedora-packager
