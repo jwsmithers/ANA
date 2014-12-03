7f3c76df81634337455451605d7fffdbadef4d54
jwsmithers <joshuasmee@hotmail.com>
2014-12-03 12:31:05 +0200

    aptget hacks added in a files. This solves gcc plugin among others

--- scripts/LinkExternalLibraries.sh
+++ scripts/LinkExternalLibraries.sh
@@ -5,8 +5,11 @@ ln -s $ROOTDIR/CORAL/$CMTCONFIG/lib/* $TopDir/DetCommon/DetCommon-18.9.0/Install
 
 #AtlasCore
 ln -s $LCG_install/pytools/1.8_python2.7/share/sources/pyxml/xml $TopDir/AtlasCore/AtlasCore-18.9.0/TestPolicy/python
+
 ln -s $LCG_install/ROOT/v5-34-21/$CMTCONFIG/lib/* $TopDir/AtlasCore/AtlasCore-18.9.0/InstallArea/$CMTCONFIG/lib/
 ln -s $LCG_install/uuid/1.42/$CMTCONFIG/lib/* $TopDir/AtlasCore/AtlasCore-18.9.0/InstallArea/$CMTCONFIG/lib/
 ln -s $LCG_install/XercesC/3.1.1p1/$CMTCONFIG/lib/* $TopDir/AtlasCore/AtlasCore-18.9.0/InstallArea/$CMTCONFIG/lib/
+ln -s $LCG_install/libunwind/1.1/$CMTCONFIG/lib/* $TopDir/AtlasCore/AtlasCore-18.9.0/InstallArea/$CMTCONFIG/lib/
+
 ln -s $LCG_install/tcmalloc/1.7p3/$CMTCONFIG/include/google $TopDir/AtlasCore/AtlasCore-18.9.0/InstallArea/include
 ln -s $LCG_install/tcmalloc/1.7p3/$CMTCONFIG/include/gperftools $TopDir/AtlasCore/AtlasCore-18.9.0/InstallArea/include
