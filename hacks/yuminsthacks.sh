881fa74ea44849553152408ab78b03e420ee18a3
jwsmithers <joshuasmee@hotmail.com>
2014-12-13 17:15:11 +0200

    syntax error in CMT script

--- hacks/yuminsthacks.sh
+++ hacks/yuminsthacks.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo yum install python-devel hdf5-devel pyutil gperftools python-4Suite-XML gcc-plugin-devel fedora-packager boost-devel
+sudo yum install subversion html2text ncurses-devel python-devel hdf5-devel pyutil gperftools python-4Suite-XML gcc-plugin-devel fedora-packager boost-devel
--- scripts/CMTStructure.sh
+++ scripts/CMTStructure.sh
@@ -4,5 +4,5 @@ do
         cmt create $TopDir/$project $project-$VERSION
         rm $TopDir/$project/$project-$VERSION/cmt/*
         echo "project $project" >> $TopDir/$project/$project-$VERSION/cmt/project.cmt
-        python $TopDir/../scripts/AddProjects.py "${project}Release ${project}Release-v*" "$project" "$LCGCMT_VERS", "$VERSION" >> $TopDir/$project/$project-$VERSION/cmt/project.cmt 
+        python $TopDir/../scripts/AddProjects.py "${project}Release ${project}Release-v*" "$project" "$LCGCMT_VERS" "$VERSION" >> $TopDir/$project/$project-$VERSION/cmt/project.cmt 
 done < Projects.txt
