e2160524a55e5a24732607ba7b526bea9fd19c48
jwsmithers <joshuasmee@hotmail.com>
2015-01-23 12:13:45 +0200

    More patches for 20.1.0

--- PreInstall/sudohacks.sh
+++ PreInstall/sudohacks.sh
@@ -3,5 +3,5 @@ cd /usr/bin; sudo ln -s gcc lcg-gcc-4.9.2; sudo ln -s g++ lcg-g++-4.9.2
 goHome
 cd /usr/lib; sudo ln -s $LCG_install/clhep/1.9.4.7/$CMTCONFIG/lib/* .
 goHome
-
+cd /usr/bin; sudo ln -s gfortan lcg-gfortran-4.9.0;sudo ln -s gfortan lcg-gfortran-4.9.1;sudo ln -s gfortan lcg-gfortran-4.9.2
 ##Boost ln of librariess should be in here?
