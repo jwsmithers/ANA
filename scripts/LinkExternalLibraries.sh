d33b9a6a5717647d81bde31be05735036f6f9ba5
jwsmithers <joshuasmee@hotmail.com>
2015-02-02 18:57:40 +0200

    More patches added

--- scripts/LinkExternalLibraries.sh
+++ scripts/LinkExternalLibraries.sh
@@ -16,8 +16,9 @@ ln -s $LCG_install/libunwind/1.1/$CMTCONFIG/lib/* $TopDir/AtlasCore/AtlasCore-$V
 ln -s $LCG_install/CppUnit/1.12.1_p1/$CMTCONFIG/lib/* $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/$CMTCONFIG/lib/
 ln -s $LCG_install/tbb/42_20140122/$CMTCONFIG/lib/*  $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/$CMTCONFIG/lib/ 
 
-ln -s $LCG_install/gperftools/2.2.1/$CMTCONFIG/include/google $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/include
-ln -s $LCG_install/gperftools/2.2.1/$CMTCONFIG/include/gperftools $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/include
+ln -s $LCG_install/tcmalloc/1.7p3/$CMTCONFIG/include/google $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/include
+ln -s $LCG_install/tcmalloc/1.7p3/$CMTCONFIG/include/gperftools $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/include
+
 ln -s $LCG_install/CppUnit/1.12.1_p1/$CMTCONFIG/include/cppunit $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/include
 ln -s $LCG_install/valgrind/3.10.0/$CMTCONFIG/include/valgrind $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/include
 ln -s $LCG_install/tbb/42_20140122/$CMTCONFIG/include/tbb $TopDir/AtlasCore/AtlasCore-$VERSION/InstallArea/include
