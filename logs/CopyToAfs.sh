01646a3ee70a3e15bdc2f504da721703ec893dd2
jwsmithers <joshuasmee@hotmail.com>
2015-02-21 16:07:47 +0200

    Logs get sent to http://jwsmith.web.cern.ch/jwsmith/

---
+++ logs/CopyToAfs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Temp way to get logs to my website to view from home.
+while true; do
+ sshpass -p $SSHPASS scp StdError.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/projects/ANA
+ sshpass -p $SSHPASS scp StdOut.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/projects/ANA 
+ sleep 10
+done
