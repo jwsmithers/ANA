5a76b766ee615dc5057f04985b85fea27833b196
jwsmithers <joshuasmee@hotmail.com>
2015-04-13 17:57:39 +0200

    COOL and CORAL entry deleted in LCG's CMakeLists files

--- 1_LCGSoftware/lcgcmake_build-armv7l-fc21-gcc49-opt-71/BuildLCG.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-LCGbuildDir=`pwd`
-cmake -DCMAKE_INSTALL_PREFIX=$LCGbuildDir/../lcgcmake_install-$CMTCONFIG-$LCGCMT_VERS $LCGbuildDir/../Lcgcmake
-make -j4 -k
---
+++ Variables.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+################################################################################################
+#                               Joshua Wyatt Smith                                             #
+#                               joshua.wyatt.smith@cern.ch                                     #
+################################################################################################
+export ROOTDIR=/home/jwsmith/HDD/ANA/ANA
+export CMTCONFIG=armv7l-fc21-gcc49-opt
+export CMAKECONFIG=armv7l-fc21-gcc49-opt
+export LCGCMT_VERS=71
+
+##LCG
+export LCG_install=$ROOTDIR/1_LCGSoftware/lcgcmake_install-$CMTCONFIG-LCGCMT_VERS
+##CORAL
+export CORALDir=$ROOTDIR/1a_CORAL/CORAL-2-4-2
+##COOL
+export COOLDir=$ROOTDIR/1b_COOL/COOL_2_9_2
+##Gaudi
+export GaudiDir=$ROOTDIR/2_GAUDI/Gaudi
