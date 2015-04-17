b0df2d7d689ee766942c1381f6d2490b85da5232
jwsmithers <joshuasmee@hotmail.com>
2015-04-17 11:34:25 +0200

    paths updated and ATLAS environment corrected

--- 3_ATLAS/Environment.sh
+++ 3_ATLAS/Environment.sh
@@ -3,27 +3,14 @@
 #                               Joshua Wyatt Smith                                             #
 #                               joshua.wyatt.smith@cern.ch                                     #
 ################################################################################################
-export ROOTDIR=/home/jwsmith/HDD
-export CMTCONFIG=armv7l-fc21-gcc49-opt
-source $ROOTDIR/lcgcmake-install-gcc49/cmt/v1r26p20140131/$CMTCONFIG/CMT/v1r26p20140131/mgr/setup.sh
+source $LCG_install/cmt/*/$CMTCONFIG/CMT/*/mgr/setup.sh
 export CMTBIN=Linux-armv7l
 export CMTSITE=STANDALONE
 export SVNROOT=svn+ssh://jwsmith@svn.cern.ch/reps/atlasoff
 export VERSION=20.1.0
 export LCGCMT_VERS=LCGCMT_71
-export TopDir=$ROOTDIR/AtlasOfflineBuild-framework/InstallArea
+export TopDir=$ROOTDIR/3_ATLAS/InstallArea
 #export AntVersion=1.9.4
-
-##COOL
-export COOLDir=$ROOTDIR/COOL/COOL_2_9_2
-export COOL_include=$ROOTDIR/COOL/$CMTCONFIG/include
-##CORAL
-export CORALDir=$ROOTDIR/CORAL/CORAL-2-4-2
-export CORAL_include=$ROOTDIR/CORAL/$CMTCONFIG/include
-##LCG
-export LCG_install=$ROOTDIR/lcgcmake-install-gcc49
-##Gaudi
-export GaudiDir=$ROOTDIR/Gaudi
 
 #Java
 export CPLUS_INCLUDE_PATH=/usr/lib/jvm/$JAVAVERS/include:/usr/lib/jvm/$JAVAVERS/include/linux:$CPLUS_INCLUDE_PATH
--- Variables.sh
+++ Variables.sh
@@ -12,7 +12,9 @@ export LCGCMT_VERS=71
 export LCG_install=$ROOTDIR/1_LCGSoftware/lcgcmake_install-$CMTCONFIG-$LCGCMT_VERS
 ##CORAL
 export CORALDir=$ROOTDIR/1a_CORAL/CORAL-2-4-2
+export CORAL_include=$CORALDir/../$CMTCONFIG/include
 ##COOL
 export COOLDir=$ROOTDIR/1b_COOL/COOL_2_9_2
+export COOL_include=$COOLDir/../$CMTCONFIG/include
 ##Gaudi
 export GaudiDir=$ROOTDIR/2_GAUDI/Gaudi
