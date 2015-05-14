c330ca164e004a10dc7039bccc11c332585820ae
jwsmithers <joshuasmee@hotmail.com>
2015-05-14 14:25:38 +0000

    new patches

--- rel_6/logs/CopyToAfs.sh
+++ rel_6/logs/CopyToAfs.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #Temp way to get logs to my website to view from home.
 while true; do
- sshpass -p $SSHPASS scp StdError.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/downloads/downloads-ANA/files
- sshpass -p $SSHPASS scp StdOut.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/downloads/downloads-ANA/files 
+/home/jwsmith/sshpass-install/bin/sshpass -p $SSHPASS scp StdError.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/downloads/downloads-ANA/files
+/home/jwsmith/sshpass-install/bin/sshpass -p $SSHPASS scp StdOut.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/downloads/downloads-ANA/files 
  python TailIt.py > StdTail.log
- sshpass -p $SSHPASS scp StdTail.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/downloads/downloads-ANA/files
+/home/jwsmith/sshpass-install/bin/sshpass -p $SSHPASS scp StdTail.log jwsmith@lxplus.cern.ch:/afs/cern.ch/user/j/jwsmith/www/downloads/downloads-ANA/files
  sleep 600
 done
