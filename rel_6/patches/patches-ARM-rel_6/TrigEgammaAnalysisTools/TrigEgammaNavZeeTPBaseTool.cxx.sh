3834856a1cbbb20a2f640e6a8c2b3ca249368138
jwsmithers <joshuasmee@hotmail.com>
2015-07-06 14:18:40 +0000

    more patches

---
+++ rel_6/patches/patches-ARM-rel_6/TrigEgammaAnalysisTools/TrigEgammaNavZeeTPBaseTool.cxx.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd $TopDir/patches/patches-ARM-rel_6/TrigEgammaAnalysisTools
+patch_dir=`pwd`
+cd $TopDir/AtlasSimulation/rel_6/Trigger/TrigAnalysis/TrigEgammaAnalysisTools/Root
+patch -b < $patch_dir/TrigEgammaNavZeeTPBaseTool.cxx.patch
